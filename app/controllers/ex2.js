1cf9eb368b7f8f742d8b71e6f0ebfe11bc212f8b
Clement-Hellot <76108212+Clement-Hellot@users.noreply.github.com>
2021-02-04 11:20:52 +0100

    style

--- app/controllers/ex2.js
+++ app/controllers/ex2.js
@@ -2,5 +2,13 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 
 export default class Ex2Controller extends Controller {
-
+    @action
+    toggleActive(service){
+    if(service.active){
+      service = !service.active;
+    }
+    else {
+      service.active;
+    }
+  }
 }
--- app/routes/ex2.js
+++ app/routes/ex2.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
 import Services from '../classes/services';
 import {datas} from '../data/datas';
+import { action } from '@ember/object';
 
 export default class Ex2Route extends Route {
     model(){
