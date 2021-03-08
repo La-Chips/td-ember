c1f1b953c855f6800bd85318a4946b1e58b2fc75
Clement Hellot <clement.hellot.etu@gmail.com>
2021-03-08 15:26:00 +0100

    catégories contact route controller template

---
+++ app/controllers/categories/contact.js
@@ -0,0 +1,17 @@
+import Controller from '@ember/controller';
+import { tracked } from '@ember/object';
+
+export default class CategoriesContactsController extends Controller {
+    @tracked datas;
+
+    get contacts() {
+
+    }
+
+    get deleteds() {
+
+    }
+    get deletedsCount() {
+
+    }
+}
