1195560efdb06969736aad68e6520d74684b1923
Clement Hellot <clement.hellot.etu@gmail.com>
2021-03-08 14:16:00 +0100

    Creations des routes catégories

--- app/router.js
+++ app/router.js
@@ -7,8 +7,16 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function () {
-  this.route('contacts', function() {
+  this.route('contacts', function () {
     this.route('add');
     this.route('update');
+  });
+  this.route('categories', function () {
+    this.route('add');
+    this.route('update', { path: '/update/:category_id' });
+    this.route('contacts', { path: '/contacts/:category_id' }, function () {
+      this.route('add');
+      this.route('update', { path: '/contacts/update/:category_id' });
+    });
   });
 });
