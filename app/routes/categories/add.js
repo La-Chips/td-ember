b86da78b6c3d38353af5b8fcf124ce3863b635ab
Clement Hellot <clement.hellot.etu@gmail.com>
2021-03-08 14:30:00 +0100

    Categories.add route et templates

--- app/routes/categories/add.js
+++ app/routes/categories/add.js
@@ -1,4 +1,13 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
 
 export default class CategoriesAddRoute extends Route {
+    model() {
+        return {};
+    }
+
+    @action save(category) { //Sauvegarde la catégorie
+        let cat = this.store.createRecord('category', category);
+        cat.save().then(() => this.transitionTo('categories'));
+    }
 }
