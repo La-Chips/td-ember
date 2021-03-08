21aa60d5bc7b4cdc24c310084d002cffcde9c9cf
Clement Hellot <clement.hellot.etu@gmail.com>
2021-03-08 14:48:00 +0100

    categories route et templates

--- app/routes/categories.js
+++ app/routes/categories.js
@@ -1,4 +1,19 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class CategoriesRoute extends Route {
+    @service router;
+
+    model() {
+        return this.store.findAll('category');
+    }
+
+    afterModel(model) {
+        this.store.findAll('contact');
+        if (model.length > 0) {
+            this.transitionTo('categories.contacts', model.get('firstObject'));
+        }
+
+    }
+
 }
