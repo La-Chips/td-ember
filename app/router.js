881e2a35c3263fedab11bf9cb52f6f74d4ae506c
Clement Hellot <clement.hellot.etu@gmail.com>
2021-03-12 15:02:27 +0100

    route order / sections

--- app/router.js
+++ app/router.js
@@ -9,4 +9,6 @@ export default class Router extends EmberRouter {
 Router.map(function () {
   this.route('Abstractroute');
   this.route('board');
+  this.route('sections');
+  this.route('order', { path: '/order/:order_id' });
 });
