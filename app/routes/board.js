df94d33b9b0179d8edb70b519339e8b99fe5f8c5
Clement Hellot <clement.hellot.etu@gmail.com>
2021-03-22 13:32:06 +0100

    login fin

--- app/routes/board.js
+++ app/routes/board.js
@@ -1,9 +1,17 @@
-import Route from '@ember/routing/route';
+import abstractroute from './abstractroute';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 
-export default class BoardRoute extends Route {
+export default class BoardRoute extends abstractroute {
+    @service userAuth;
+
+    model() {
+        let user = this.userAuth.user;
+    }
+
     @action logout() {
+        this.userAuth.logout();
         this.transitionTo('index');
     }
 }
